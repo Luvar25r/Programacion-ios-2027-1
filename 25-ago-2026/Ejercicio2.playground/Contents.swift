8b9814a89dd222dfeb5bef3ea615662fe7386c4c
Luvar25r <luvar25@comunidad.unam.mx>
2026-08-25 14:48:23 -0600

    Ejercicio 1 y 2 completos, Ejercicio 3 incompleto

---
+++ 25-ago-2026/Ejercicio2.playground/Contents.swift
@@ -0,0 +1,14 @@
+let name = "Luis Vadillo"
+
+let favouriteQuote = "Mi frase favorita es \"Si fuera facil todo el mundo lo haría.\""
+print (favouriteQuote)
+
+
+let emptyString = ""
+
+
+if emptyString.isEmpty {
+    print("No hay nada aqui")
+} else {
+    print("No está tan vacio como pensaba")
+}
