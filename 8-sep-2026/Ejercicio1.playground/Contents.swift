24c0319f8b94644ca4322218a2f2238d1dfe827c
Luvar25r <luvar25@comunidad.unam.mx>
2026-09-08 14:20:44 -0600

    Ejercicio 1 y 2 del 8 de Septiembre

---
+++ 8-sep-2026/Ejercicio1.playground/Contents.swift
@@ -0,0 +1,15 @@
+struct Person {
+    let name: String
+    var age: Int
+    
+    
+}
+
+var jay = Person(name: "Jay", age: 24)
+
+var myFriend = jay
+
+jay.age += 1
+
+print(jay.age)
+print(myFriend.age)
